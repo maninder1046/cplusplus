ee376e0cfe8211747c72359c97e83b8e64552a0c
maninder1046 <90817762+maninder1046@users.noreply.github.com>
2021-11-24 23:27:31 +0530

    Create t7.cpp

---
+++ practical7/t7.cpp
@@ -0,0 +1,21 @@
+;
+	cout<<"Enter size of the array:-\n";
+	cin>>n;
+	char a[n][n];
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<(n-i-1);j++)
+			a[i][j]=' ';
+		for(j=n-1;j>=(n-i-1);j--)
+			a[i][j]='*';
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			cout<<a[i][j]<<" ";
+		}
+		cout<<'\n';
+	}
+	return 0;
+}
