47b75b26f610e25141ccca24f1bb63941ebbe8a5
maninder1046 <90817762+maninder1046@users.noreply.github.com>
2021-11-24 23:28:08 +0530

    Create t8.cpp

---
+++ practical7/t8.cpp
@@ -0,0 +1,29 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,i,j;
+	cout<<"Enter size of the array:-\n";
+	cin>>n;
+	char a[n][n];
+	for(i=n-1;i>=0;i--)
+	{
+		for(j=i;j>=0;j--)
+		{
+			a[i][j]='*';
+		}
+	}
+	for(i=n-1;i>=0;i--)
+	{
+		for(j=i;j>=0;j--)
+		{
+			cout<<a[i][j]<<" ";
+		}
+		cout<<'\n';
+	}
+	return 0;
+}
+© 2021 GitHub, Inc.
+Terms
