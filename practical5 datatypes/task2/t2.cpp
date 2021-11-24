c35c8d2ecebb381ca4afae4fed2099c92ab85ee6
maninder1046 <90817762+maninder1046@users.noreply.github.com>
2021-11-24 22:22:57 +0530

    Create t2.cpp

---
+++ practical5 datatypes/task2/t2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"size of bool = "<<sizeof(bool)<<endl;
+  cout<<"size of integer = "<<sizeof(int)<<endl;
+  cout<<"size of float = "<<sizeof(float)<<endl;
+  cout<<"size of short = "<<sizeof(short)<<endl;
+  cout<<"size of long = "<<sizeof(long)<<endl;
+  cout<<"size of double = "<<sizeof(double)<<endl;
+  cout<<"size of wild char = "<<sizeof(wchar_t)<<endl;
+  cout<<"size of long double = "<<sizeof(long double)<<endl;
+}
