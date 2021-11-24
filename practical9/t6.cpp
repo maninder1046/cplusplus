a9803b8cb96e77625f56c6f6cef8dd7d816a1f0a
maninder1046 <90817762+maninder1046@users.noreply.github.com>
2021-11-24 23:51:35 +0530

    Create t6.cpp

---
+++ practical9/t6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main () 
+{
+cout<<"maninder singh"<<endl;
+
+cout<<setw(15)<<2001110<<endl;
+
+cout<<setfill('*')<<setw(15)<<20011160<<endl;
+return 0;
+}
